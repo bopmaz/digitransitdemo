e1253d0a5c4257af28d03a3f5003b30d68ef500a
Mint <tuanminh.nguyen@hotmail.fi>
2023-09-06 02:11:56 +0300

    feat: Add view

--- app/src/main/java/com/mint/digitransitdemo/presentation/StopsScreen.kt
+++ app/src/main/java/com/mint/digitransitdemo/presentation/StopsScreen.kt
@@ -1,4 +1,149 @@
 package com.mint.digitransitdemo.presentation
 
-class StopsScreen {
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.paging.LoadState
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.mint.digitransitdemo.domain.BaseStop
+import com.mint.digitransitdemo.ui.theme.BodyStyle
+import com.mint.digitransitdemo.ui.theme.CardHeight
+import com.mint.digitransitdemo.ui.theme.DefaultElevation
+import com.mint.digitransitdemo.ui.theme.MapHeight
+import com.mint.digitransitdemo.ui.theme.Padding16
+import com.mint.digitransitdemo.ui.theme.Padding8
+import com.mint.digitransitdemo.ui.theme.TitleStyle
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun StopsScreen(
+    viewModel: StopsViewModel,
+    onSelectedStop: (id: String) -> Unit
+) {
+    val state by viewModel.state.collectAsState()
+    val stopList = viewModel.stopsPagingData.collectAsLazyPagingItems()
+    val refreshing by viewModel.isRefreshing.collectAsStateWithLifecycle()
+
+    val pullRefreshState = rememberPullRefreshState(refreshing, onRefresh = { stopList.refresh() })
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+
+        if (state.isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center)
+            )
+        } else {
+            val cameraPositionState = rememberCameraPositionState {
+                position = CameraPosition.fromLatLngZoom(
+                    LatLng(
+                        state.currentLocation.latitude,
+                        state.currentLocation.longitude
+                    ), 15f
+                )
+            }
+
+            Column {
+                GoogleMap(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(MapHeight),
+                    cameraPositionState = cameraPositionState
+                ) {
+                    stopList.itemSnapshotList.items.forEach {
+                        Marker(
+                            state = MarkerState(position = LatLng(it.lat, it.lon)),
+                            title = it.name,
+                        )
+                    }
+
+                }
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .pullRefresh(pullRefreshState)
+                ) {
+
+                    if (stopList.loadState.refresh == LoadState.Loading) {
+                        item {
+                            Text(
+                                text = "Waiting for items to load",
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .wrapContentWidth(Alignment.CenterHorizontally)
+                            )
+                        }
+                    }
+
+                    items(count = stopList.itemCount) { index ->
+                        val stop = stopList[index] ?: BaseStop()
+                        StopCard(
+                            stop = stop,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(CardHeight)
+                                .clickable { onSelectedStop(stop.gtfsId) }
+                        )
+                    }
+                }
+            }
+        }
+
+        PullRefreshIndicator(
+            refreshing = refreshing,
+            state = pullRefreshState,
+            modifier = Modifier.align(alignment = Alignment.TopCenter)
+        )
+    }
+}
+
+@Composable
+fun StopCard(stop: BaseStop, modifier: Modifier) {
+    Card(
+        elevation = DefaultElevation,
+        modifier = modifier
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = Padding16, vertical = Padding8
+                )
+        ) {
+            Text(
+                text = stop.name,
+                style = TitleStyle
+            )
+            Text(
+                text = "STOP ID: ${stop.gtfsId}",
+                style = BodyStyle
+            )
+        }
+    }
 }
--- app/src/main/java/com/mint/digitransitdemo/presentation/StopsViewModel.kt
+++ app/src/main/java/com/mint/digitransitdemo/presentation/StopsViewModel.kt
@@ -1,13 +1,94 @@
 package com.mint.digitransitdemo.presentation
 
+import android.location.Location
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.mint.digitransitdemo.domain.BaseStop
+import com.mint.digitransitdemo.domain.DetailStop
 import com.mint.digitransitdemo.domain.GetStopUseCase
 import com.mint.digitransitdemo.domain.GetStopsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class StopViewModel(
+@HiltViewModel
+class StopsViewModel @Inject constructor(
     private val getStopUseCase: GetStopUseCase,
     private val getStopsUseCase: GetStopsUseCase
 ) : ViewModel() {
-    
 
+    private val _isRefreshing = MutableStateFlow(false)
+    val isRefreshing: StateFlow<Boolean>
+        get() = _isRefreshing.asStateFlow()
+    private val _state = MutableStateFlow(StopsState())
+    val state = _state.asStateFlow()
+    var stopsPagingData: Flow<PagingData<BaseStop>> = flowOf()
+
+    init {
+        viewModelScope.launch {
+            _state.update {
+                it.copy(
+                    isLoading = true
+                )
+            }
+        }
+    }
+
+    fun getStops() {
+        viewModelScope.launch {
+            _state.update {
+                stopsPagingData = getStopsUseCase.execute(
+                    lat = it.currentLocation.latitude,
+                    lon = it.currentLocation.longitude,
+                    radius = DEFAULT_RADIUS
+                ).cachedIn(viewModelScope)
+
+                it.copy(
+                    isLoading = false
+                )
+            }
+        }
+    }
+
+    fun selectStop(id: String) {
+        viewModelScope.launch {
+            _state.update {
+                it.copy(
+                    selectedStop = getStopUseCase.execute(id)
+                )
+            }
+        }
+    }
+
+    fun updateCurrentLocation(lat: Double, lon: Double) {
+        val updatedLocation = _state.value.currentLocation
+        updatedLocation.latitude = lat
+        updatedLocation.longitude = lon
+
+        viewModelScope.launch {
+            _state.update {
+                it.copy(
+                    currentLocation = updatedLocation
+                )
+            }
+        }
+    }
 }
+
+data class StopsState(
+    val isLoading: Boolean = false,
+    val selectedStop: DetailStop? = null,
+    val currentLocation: Location = Location(""),
+    val shouldLoadMore: Boolean = true,
+    val pageId: String = ""
+)
+
+private const val DEFAULT_RADIUS = 500
